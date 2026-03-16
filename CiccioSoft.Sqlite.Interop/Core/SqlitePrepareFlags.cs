657f603c944eac0207bf03e1274ea99d944cc715
Francesco Crimi <francrim@hotmail.com>
2026-03-16 13:32:22 +0100

    Add typed sqlite prepare flags and use Persistent flag

---
+++ CiccioSoft.Sqlite.Interop/Core/SqlitePrepareFlags.cs
@@ -0,0 +1,18 @@
+// Copyright (c) 2026 Francesco Crimi
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+using System;
+
+namespace CiccioSoft.Sqlite.Interop;
+
+[Flags]
+public enum SqlitePrepareFlags : uint
+{
+    None = 0,
+    Persistent = 0x01,
+    Normalize = 0x02,
+    NoVtab = 0x04
+}
